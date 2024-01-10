2b4d9eac53c72de264e3ab20121107253f9e219f
Julien Thomas <julthoma@1B3.42angouleme.fr>
2024-01-10 18:44:25 +0100

    first stable

--- main.c
+++ main.c
@@ -14,110 +14,113 @@
 
 #include "pipex.h"
 
-static void ft_free_tab(char **tab)
+static void	ft_free(char **str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (tab[i])
-        free(tab[i++]);
-    free(tab);
+	i = 0;
+	while (str[i])
+	{
+		free(str[i++]);
+	}
+	free(str);
 }
 
-static char *ft_getcmd_path(char *name, char **envp)
+// TODO: rework this function
+static char	*get_path(char *cmd, char **envp)
 {
-    char *path;
-    char **paths;
-    int i;
+	int		i;
+	char	*path;
+	char	**paths;
 
-    i = 0;
-    while (ft_strncmp(envp[i], "PATH=", 5) != 0)
-        i++;
-    path = ft_strdup(envp[i] + 5);
-    paths = ft_split(path, ':');
-    i = 0;
-    while (paths[i])
-    {
-        path = ft_strjoin(paths[i], "/");
-        path = ft_strjoin(path, name);
-        if (access(path, X_OK) == 0)
-        {
-            ft_free_tab(paths);
-            return (path);
-        }
-        free(path);
-        i++;
-    }
-    ft_free_tab(paths);
-    return (NULL);
+	i = 0;
+	while (ft_strncmp(envp[i], "PATH=", 5) != 0)
+		i++;
+	path = ft_strdup(envp[i] + 5);
+	paths = ft_split(path, ':');
+	i = 0;
+	while (paths[i])
+	{
+		path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(path, cmd);
+		if (access(path, X_OK) == 0)
+		{
+			return (path);
+		}
+		free(path);
+		i++;
+	}
+	ft_printf("command not found: %s\n", cmd);
+	exit(1);
 }
 
-static void exec_command(char **cmd, char **envp)
+static void	exec_command(int fdin, char **cmd, char **envp, int fdou)
 {
-    char *cmd_path;
+	int		pid;
+	char	*path;
 
-    cmd_path = ft_getcmd_path(cmd[0], envp);
-    if (cmd_path == NULL)
-    {
-        ft_printf("command not found\n");
-        exit(1);
-    }
-    execve(cmd_path, cmd, envp);
-    perror("execve");
-    exit(1);
+	pid = fork();
+	if (pid < 0)
+	{
+		return ;
+	}
+	else if (pid == 0)
+	{
+		path = get_path(cmd[0], envp);
+		dup2(fdin, STDIN_FILENO);
+		dup2(fdou, STDOUT_FILENO);
+		execve(path, cmd, envp);
+		free(path);
+	}
+	close(fdin);
+	close(fdou);
+	waitpid(pid, NULL, 0);
 }
 
-static void pipex(int fd[2], char **cmd1, char **cmd2, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-    int pid;
+	int		input;
+	int		output;
+	int		fd[2];
+	char	**cmd;
 
-    (void)envp;
-    pid = fork();
-    if (pid == 0)
-    {
-        dup2(fd[0], STDOUT_FILENO);
-        close(fd[1]);
-        //execvp(cmd1[0], cmd1);
-        exec_command(cmd1, envp);
-    }
-    else if (pid == 1)
-    {
-        dup2(fd[1], STDIN_FILENO);
-        close(fd[0]);
-        //execvp(cmd2[0], cmd2);
-        exec_command(cmd2, envp);
-    }
-    else if (pid == -1)
-    {
-        perror("fork");
-    }
-
-    ft_free_tab(cmd1);
-    ft_free_tab(cmd2);
-}
-
-int main(int argc, char **argv, char **envp)
-{
-    int pipefd[2];
-    int pid;
-
-    (void)argc;
-    //if (argc != 5)
-    //    return (1);
-    pipe(pipefd);
-    pid = fork();
-    if (pid == 0)
-    {
-        pipex(pipefd, ft_split(argv[1], ' '), ft_split(argv[2], ' '), envp);
-    }
-    else if (pid == 1)
-    {
-        ft_printf("not dev\n");
-    }
-    else if (pid == -1)
-    {
-        perror("fork");
-        return (1);
-    }
-    return (0);
+	if (argc == 5)
+	{
+		if (pipe(fd) == -1)
+		{
+			perror("pipe creation");
+			return (2);
+		}
+		input = open(argv[1], O_RDONLY);
+		if (input == -1)
+		{
+			perror("input file");
+			return (2);
+		}
+		output = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (output == -1)
+		{
+			perror("output file");
+			return (2);
+		}
+		cmd = ft_split(argv[2], ' ');
+		if (cmd == NULL)
+		{
+			perror("split");
+			return (2);
+		}
+		exec_command(input, cmd, envp, fd[1]);
+		ft_free(cmd);
+		cmd = ft_split(argv[3], ' ');
+		if (cmd == NULL)
+		{
+			perror("split");
+			return (2);
+		}
+		exec_command(fd[0], cmd, envp, output);
+		ft_free(cmd);
+		close(input);
+		close(output);
+	}
+	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -1,7 +1,21 @@
-#ifndef PIPEX_PIPEX_H
-# define PIPEX_PIPEX_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julthoma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 18:27:30 by julthoma          #+#    #+#             */
+/*   Updated: 2024/01/10 18:27:30 by julthoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
 
 # include "libft/include/libft.h"
 # include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
 
 #endif
